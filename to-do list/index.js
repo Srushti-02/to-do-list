f5d4a10b2355b3bfdb9ef0fb3423c41368f97913
Srushti-02 <srushta.272002@gmail.com>
2021-10-29 17:24:30 +0530

    add&clear

--- to-do list/index.js
+++ to-do list/index.js
@@ -1,3 +1,5 @@
+/**------dark-mode------ */
+
 var body = document.querySelector('body')
 var button = document.querySelector('.slider')
 
@@ -8,3 +10,61 @@ const callbackfunc = (event) =>{
 }
 
 button.addEventListener('click', callbackfunc)
+
+
+/**-----add-task------ */
+var chores =[]
+
+var button = document.querySelector('.add_button')
+var input = document.querySelector('.input__task')
+var list = document.querySelector('ul')
+
+const add = (event) => {
+  const inputValue = input.value
+  if(chores.includes(inputValue)){
+    alert(`"${inputValue}" already exists`)
+  }
+
+  else if (input.value == "") {
+    alert(`task cannot be blank`)
+  }
+
+  else{
+    chores.push(inputValue)
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(inputValue)
+    element.appendChild(textNode)
+    list.appendChild(element)
+
+  }
+}
+
+// const callbackfunc1 = (event) => {
+//   const inputValue = input.value
+//   const element = document.createElement('li')
+//   const textNode = document.createTextNode(inputValue)
+//   element.appendChild(textNode)
+//   list.appendChild(element)
+// }
+
+
+
+button.addEventListener('click', add)
+
+/** ----- CLEAR----- */
+var button1 = document.querySelector('.clear')
+
+const clearAll =  (event) =>{
+  const remove =  list.remove()
+}
+button1.addEventListener('click', clearAll)
+
+/** -----all task----- */
+
+/** ----completed----- */
+
+/** ----pending----- */
+
+/** ----deleteOne---- */
+
+/** ----checkbox---- */
